656993d4263e85215dcdafe1f06fbdd0acedd856
youone04 <yudi.gunaone87@gmail.com>
2022-07-18 07:54:41 +0700

    push project

---
+++ src/components/RequireAuth.js
@@ -0,0 +1,19 @@
+import { useLocation, Navigate, Outlet } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+const RequireAuth = ({ allowedRoles }) => {
+    const { auth } = useAuth();
+    const location = useLocation();
+
+    console.log(auth?.role?.find(r => allowedRoles?.includes(r)))
+
+    return (
+        auth?.role?.find(r => allowedRoles?.includes(r))
+            ? <Outlet />
+            : auth?.name
+                ? <Navigate to="/unauthorized" state={{ from: location }} replace />
+                : <Navigate to="/login" state={{ from: location }} replace />
+    );
+}
+
+export default RequireAuth;
